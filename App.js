04690364a04c57569cb2c2c6621d7e88d3cb3aa1
Prince <ashnand2000@gmail.com>
2024-08-19 19:18:59 +0530

    Font helper added

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import { getFontFamily } from './assets/fonts/helper';
 
 
 
 const App =() => {
   return (
     <SafeAreaView>
-      <Text>Business Tycoon</Text>
+      <Text style = {{fontSize:70, fontFamily: getFontFamily(700)}}>Business Tycoon</Text>
     </SafeAreaView>
   );
 }
