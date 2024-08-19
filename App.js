328f56ba19e466a3626858dc3dcd8b61b37700f5
Prince <ashnand2000@gmail.com>
2024-08-19 18:57:36 +0530

    Project Intialization

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+
+
+const App =() => {
+  return (
+    <SafeAreaView>
+      <Text>Business Tycoon</Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
