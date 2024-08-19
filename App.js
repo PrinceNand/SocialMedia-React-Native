32cee8f85c621c1091cd14e503135df1a2e37e9c
Prince <ashnand2000@gmail.com>
2024-08-19 20:03:53 +0530

    FontawesomeIcon and number added

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import { getFontFamily } from './assets/fonts/helper';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import Title from './components/Title/Title';
+import globalStyle from './globalStyle';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 
 
 const App =() => {
   return (
     <SafeAreaView>
-      <View>
-      <Title title={"Let's Explore"} />
+      <View style={globalStyle.header}>
+        <Title title={'Let’s Explore'} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
